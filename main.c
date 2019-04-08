94a4783454e3f51dc7d8316e270b662af998ce9d
Wolf Gp <45176998+haruka98@users.noreply.github.com>
2019-04-08 15:49:46 +0200

    v1.0

--- main.c
+++ main.c
@@ -5,19 +5,60 @@
 
 int main(int argc, char* argv[]) {
 	//setup needed vars
-	char acc[20] = "";	//contains account name
 	int request_id = 10000; //send id start
+	char item_amount[20] = "1";	//item_amount, should not exceed stack limit
+	char input_str[20] = "";	//string for the user input
+	char entire_str[256] = "";	//string to store the entire sql command
 	
 	//Open the output file
 	FILE* send_items_file;
 	send_items_file = fopen("send_items.sql", "a");
 	
 	//Test additional start info
-	if(argc > 1) {
-		strcpy(acc, argv[1]);	//use the defined account name
-		if(argc > 2) {
-			request_id = (int) strtol(argv[2], (char **)NULL, 10);	//use the defined request_id to start
+	if(argc > 2) {
+		request_id = (int) strtol(argv[2], (char**) NULL, 10);	//use the defined request_id to start
+	}
+	
+	while(TRUE) {	//main loop
+		
+		//init string and add request_id
+		sprintf(entire_str, "INSERT INTO \"item_receivable\" VALUES (%d, 1, '2001-1-1 11:11:11.00001', '2001-1-1 11:11:11.00001', '", request_id);
+		
+		//test for account name
+		if(argc == 1) {
+			printf("Account name:\n");
+			scanf("%s", &input_str);
+			if(strcmp("exit", input_str) == 0) {
+				break;
+			} else {
+				strcat(entire_str, input_str);
+			}
+		} else {
+			strcat(entire_str, argv[1]);
 		}
+		strcat(entire_str, "', ");
+		
+		//enter item id
+		printf("Item ID:\n");
+		scanf("%s", &input_str);
+		if(strcmp("exit", input_str) == 0) {
+			break;
+		} else {
+			strcat(entire_str, input_str);
+		}
+		strcat(entire_str, ", ");
+		
+		//enter item amount
+		printf("Enter item amount:\n");
+		scanf("%s", &input_str);
+		if(strcmp("exit", input_str) == 0) {
+			break;
+		} else {
+			strcat(entire_str, input_str);
+		}
+		fprintf(send_items_file, "%s, 1010, 'spawn_items', 200, 1, '', 0, 0, 1);\n", entire_str);
+		
+		request_id++;
 	}
 	
 	
